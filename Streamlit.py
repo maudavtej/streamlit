fac99f8c6f9ef321658b19bdab1e5eaa13f777a7
maudavtej <162544160+maudavtej@users.noreply.github.com>
2025-11-02 16:31:48 -0600

    Add basic Streamlit app for predator-prey simulation

---
+++ Streamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("SIMULACIÓN DEPREDADOR-PRESA")
+st.write("Prueba del front end")
+
+slider= st.slider("Selecciona un valor", 1,2,3,4,5)
+st.write("Valor seleccionado", slider)
+
