85b946cfa8163faa46477a37be3e275da275b7d9
maudavtej <tejeromauricio80@gmail.com>
2025-11-02 16:40:06 -0600

    prueba streamlit sse corrige slide

--- Streamlit.py
+++ Streamlit.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("SIMULACIÓN DEPREDADOR-PRESA")
 st.write("Prueba del front end")
 
-slider= st.slider("Selecciona un valor", 1,2,3,4,5)
+slider= st.slider("Selecciona un valor", 1,2,3)
 st.write("Valor seleccionado", slider)
 
